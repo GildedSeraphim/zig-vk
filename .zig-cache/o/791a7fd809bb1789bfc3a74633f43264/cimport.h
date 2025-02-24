72fb2ad2b4471faf54c56a1303cd60154cfa88fc
GildedSeraphim <nathanishaaz@gmail.com>
2025-02-23 18:11:15 -0600

    finished initialization of vulkan and enabled validation layers

---
+++ .zig-cache/o/791a7fd809bb1789bfc3a74633f43264/cimport.h
@@ -0,0 +1,6 @@
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#define GLFW_INCLUDE_VULKAN
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
---
+++ src/render/vulkan.zig
@@ -0,0 +1,71 @@
+const std = @import("std");
+const c = @import("../clibs.zig");
+
+const Allocator = std.mem.Allocator;
+
+const builtin = @import("builtin");
+const debug = (builtin.mode == .Debug);
+
+const validation_layers: []const [*c]const u8 = if (!debug) &[0][*c]const u8{} else &[_][*c]const u8{
+    "VK_LAYER_KHRONOS_validation",
+};
+
+pub const Error = error{
+    out_of_host_memory,
+    out_of_device_memory,
+    initialization_failed,
+    layer_not_present,
+    extension_not_present,
+    incompatible_driver,
+    unknown_error,
+};
+
+pub fn mapError(result: c_int) !void {
+    return switch (result) {
+        c.VK_SUCCESS => {},
+        c.VK_ERROR_OUT_OF_HOST_MEMORY => Error.out_of_host_memory,
+        c.VK_ERROR_OUT_OF_DEVICE_MEMORY => Error.out_of_device_memory,
+        c.VK_ERROR_INITIALIZATION_FAILED => Error.initialization_failed,
+        c.VK_ERROR_LAYER_NOT_PRESENT => Error.layer_not_present,
+        c.VK_ERROR_EXTENSION_NOT_PRESENT => Error.extension_not_present,
+        c.VK_ERROR_INCOMPATIBLE_DRIVER => Error.incompatible_driver,
+        else => Error.unknown_error,
+    };
+}
+
+pub const Instance = struct {
+    handle: c.VkInstance,
+
+    const app_info: c.VkApplicationInfo = .{
+        .sType = c.VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pNext = null,
+        .pApplicationName = "Voxel engine",
+        .applicationVersion = c.VK_MAKE_VERSION(1, 0, 0),
+        .pEngineName = "sabr engine",
+        .engineVersion = c.VK_MAKE_VERSION(1, 0, 0),
+        .apiVersion = c.VK_API_VERSION_1_3,
+    };
+
+    const create_info: c.VkInstanceCreateInfo = .{
+        .sType = c.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pNext = null,
+        .pApplicationInfo = &app_info,
+        .enabledLayerCount = @intCast(validation_layers.len),
+        .ppEnabledLayerNames = validation_layers.ptr,
+        .enabledExtensionCount = 0,
+        .ppEnabledExtensionNames = null,
+    };
+
+    var instance: c.VkInstance = undefined;
+
+    pub fn create() !Instance {
+        mapError(try c.vkCreateInstance(&create_info, null, &instance));
+        return Instance{
+            .handle = instance,
+        };
+    }
+
+    pub fn destroy(i: Instance) void {
+        mapError(try c.vkDestroyInstance(i.handle, null));
+    }
+};
