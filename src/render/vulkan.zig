1997931530d9e0b2e63bab7057356ce1af946036
GildedSeraphim <nathanishaaz@gmail.com>
2025-10-08 21:17:22 -0500

    Device backup

--- src/render/vulkan.zig
+++ src/render/vulkan.zig
@@ -15,6 +15,15 @@ pub fn init(alloc: Allocator) !void {
     defer window.destroy();
     const extensions = w.getExtensions();
 
+    const validation_layers: []const [*c]const u8 = &[_][*c]const u8{
+        "VK_LAYER_KHRONOS_validation",
+    };
+
+    const device_extensions: []const [*c]const u8 = &[_][*c]const u8{
+        c.VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+        c.VK_KHR_swapchain,
+    };
+
     // Instance --------------------------------------------------------------------
     const application_info: c.VkApplicationInfo = .{
         .sType = c.VK_STRUCTURE_TYPE_APPLICATION_INFO,
@@ -28,8 +37,8 @@ pub fn init(alloc: Allocator) !void {
     const instance_create_info: c.VkInstanceCreateInfo = .{
         .sType = c.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
         .pApplicationInfo = &application_info,
-        .enabledLayerCount = 0,
-        .ppEnabledLayerNames = null, // Validation layers can be set up later
+        .enabledLayerCount = @intCast(validation_layers.len),
+        .ppEnabledLayerNames = validation_layers.ptr, // Validation layers can be set up later
         .enabledExtensionCount = @intCast(extensions.len),
         .ppEnabledExtensionNames = extensions.ptr,
     };
@@ -49,9 +58,10 @@ pub fn init(alloc: Allocator) !void {
 
     var j: u32 = 0;
     var found: bool = false;
+    var physical_device: c.VkPhysicalDevice = undefined;
     while (j < physical_device_count) {
         if (isSuitable(physical_device_list[j])) {
-            //const physical_device: c.VkPhysicalDevice = physical_device_list[j];
+            physical_device = physical_device_list[j];
             found = true;
             std.debug.print("Chosen ID :: {d} / {d}\n", .{ j + 1, physical_device_count });
         }
@@ -62,8 +72,81 @@ pub fn init(alloc: Allocator) !void {
         return error.NO_VALID_GPU;
     }
 
-    // Device ---------------------------------------------------------------------
+    std.debug.print("{any}\n", .{@as([*]u64, @ptrCast(@alignCast(physical_device)))[0..4]}); // Luccie madness, code used by the deranged.
 
+    // Surface ---------------------------------------------------------------------
+    var surface: c.VkSurfaceKHR = undefined;
+    _ = c.glfwCreateWindowSurface(instance, @ptrCast(window.raw), null, &surface);
+    defer c.vkDestroySurfaceKHR(instance, surface, null);
+
+    // Device ----------------------------------------------------------------------
+    const present_queue_index = try presentQueue(physical_device, alloc, surface);
+    const graphics_queue_index = try graphicsQueue(physical_device, alloc);
+
+    const priorities: f32 = 1.0;
+
+    const graphics_queue_create_info: c.VkDeviceQueueCreateInfo = .{
+        .sType = c.VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+        .queueCount = 1,
+        .pQueuePriorities = &priorities,
+        .queueFamilyIndex = present_queue_index,
+    };
+    const present_queue_create_info = c.VkDeviceQueueCreateInfo{
+        .sType = c.VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+        .queueCount = 1,
+        .pQueuePriorities = &priorities,
+        .queueFamilyIndex = graphics_queue_index,
+    };
+
+    const queues: []const c.VkDeviceQueueCreateInfo = &.{ graphics_queue_create_info, present_queue_create_info };
+
+    var device_features: c.VkPhysicalDeviceFeatures = .{};
+
+    _ = c.vkGetPhysicalDeviceFeatures(physical_device, &device_features);
+
+    const features = try alloc.alloc(c.VkPhysicalDeviceFeatures, 1);
+    defer alloc.free(features);
+
+    const device_create_info = c.VkDeviceCreateInfo{
+        .sType = c.VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+        .pNext = null,
+        .queueCreateInfoCount = @intCast(queues.len),
+        .pQueueCreateInfos = @ptrCast(queues.ptr),
+        .enabledExtensionCount = @intCast(device_extensions.len),
+        .ppEnabledExtensionNames = @ptrCast(device_extensions.ptr),
+        .pEnabledFeatures = @ptrCast(features),
+    };
+
+    var device: c.VkDevice = undefined;
+    _ = c.vkCreateDevice(physical_device, &device_create_info, null, &device);
+    defer c.vkDestroyDevice(device, null);
+
+    // Swapchain ------------------------------------------------------------------
+    const extent = c.VkExtent2D{ .width = 800, .height = 600 };
+    const swapchain_create_info = c.VkSwapchainCreateInfoKHR{
+        .sType = c.VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+        .surface = surface,
+        .minImageCount = 4,
+        .imageFormat = c.VK_FORMAT_R8G8B8A8_SRGB,
+        .imageColorSpace = c.VK_COLOR_SPACE_SRGB_NONLINEAR_KHR,
+        .imageExtent = extent,
+        .imageArrayLayers = 1,
+        .imageUsage = c.VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+        .presentMode = c.VK_PRESENT_MODE_IMMEDIATE_KHR,
+        .imageSharingMode = c.VK_SHARING_MODE_CONCURRENT,
+        .queueFamilyIndexCount = @intCast(queues.len),
+    };
+
+    var swapchain: c.VkSwapchainKHR = undefined;
+    _ = c.vkCreateSwapchainKHR(device, &swapchain_create_info, null, &swapchain);
+    defer c.vkDestroySwapchainKHR(device, swapchain, null);
+
+    const images = try alloc.alloc(c.VkImage, 4);
+    var swap_count: u32 = undefined;
+    defer alloc.free(images);
+    _ = c.vkGetSwapchainImagesKHR(device, swapchain, &swap_count, @ptrCast(images));
+
+    std.debug.print("Swap Count :: {d}\n", .{swap_count});
 }
 
 // Helper Functions ---------------------------------------------------------------
@@ -82,4 +165,61 @@ fn isSuitable(device: c.VkPhysicalDevice) bool {
     }
 
     return is_suitable;
+}
+
+fn getQueueFamilyProperties(phys_dev: c.VkPhysicalDevice, alloc: Allocator) ![]const c.VkQueueFamilyProperties {
+    var count: u32 = undefined;
+    _ = c.vkGetPhysicalDeviceQueueFamilyProperties(phys_dev, &count, null);
+
+    const queue_list = try alloc.alloc(c.VkQueueFamilyProperties, count);
+
+    _ = c.vkGetPhysicalDeviceQueueFamilyProperties(phys_dev, &count, @ptrCast(queue_list));
+
+    std.debug.print("Queue count:: {d}\n", .{count});
+    return queue_list;
+}
+
+fn graphicsQueue(phys_dev: c.VkPhysicalDevice, alloc: Allocator) !u32 {
+    const queue_families = try getQueueFamilyProperties(phys_dev, alloc);
+    defer alloc.free(queue_families);
+
+    var graphics_queue: ?u32 = null;
+
+    for (queue_families, 1..) |family, index| {
+        if (graphics_queue) |_| {
+            break;
+        }
+
+        if ((family.queueFlags & c.VK_QUEUE_GRAPHICS_BIT) != 0x0) {
+            graphics_queue = @intCast(index);
+        }
+    }
+
+    std.debug.print("Graphics Queue Index:: {d}\n", .{graphics_queue.?});
+
+    return graphics_queue.?;
+}
+
+fn presentQueue(phys_dev: c.VkPhysicalDevice, alloc: Allocator, surface: c.VkSurfaceKHR) !u32 {
+    const queue_families = try getQueueFamilyProperties(phys_dev, alloc);
+    defer alloc.free(queue_families);
+
+    var present_queue: ?u32 = null;
+
+    for (queue_families, 0..) |_, index| {
+        if (present_queue) |_| {
+            break;
+        }
+
+        var support: u32 = undefined;
+        _ = c.vkGetPhysicalDeviceSurfaceSupportKHR(phys_dev, @intCast(index), surface, &support);
+
+        if (support == c.VK_TRUE) {
+            present_queue = @intCast(index);
+        }
+    }
+
+    std.debug.print("Present Queue Index:: {d}\n", .{present_queue.?});
+
+    return present_queue.?;
 }
